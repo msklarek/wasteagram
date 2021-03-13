5c0a2de60f4e35e79570e247abfb7884a0371e84
Malgorzata Sinha <sklarekm@oregonstate.edu>
2021-03-13 12:10:53 -0800

    before using form

--- lib/screens/add_post.dart
+++ lib/screens/add_post.dart
@@ -1,7 +1,12 @@
 import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter/services.dart';
+import 'package:location/location.dart';
+import '../location.dart';
+import 'package:path/path.dart' as Path;
 
 class AddPost extends StatefulWidget {
   @override
@@ -10,6 +15,13 @@ class AddPost extends StatefulWidget {
 
 class _AddPostState extends State<AddPost> {
   File image;
+  LocationData locationData;
+
+  @override
+  void initState() {
+    super.initState();
+    retrieveLocation();
+  }
 
   final picker = ImagePicker();
   Future getImage() async {
@@ -24,10 +36,50 @@ class _AddPostState extends State<AddPost> {
     setState(() {});
   }
 
+  Future uploadImage() async {
+    StorageReference storageReference =
+        FirebaseStorage.instance.ref().child(Path.basename(image.path));
+    StorageUploadTask uploadTask = storageReference.putFile(image);
+    await uploadTask.onComplete;
+    final url = await storageReference.getDownloadURL();
+    print(url);
+    setState(() {});
+  }
+
+  void retrieveLocation() async {
+    var locationService = Location();
+    try {
+      var _serviceEnabled = await locationService.serviceEnabled();
+      if (!_serviceEnabled) {
+        _serviceEnabled = await locationService.requestService();
+        if (!_serviceEnabled) {
+          print('Failed to enable service. Returning.');
+          return;
+        }
+      }
+
+      var _permissionGranted = await locationService.hasPermission();
+      if (_permissionGranted == PermissionStatus.denied) {
+        _permissionGranted = await locationService.requestPermission();
+        if (_permissionGranted != PermissionStatus.granted) {
+          print('Location service permission not granted. Returning.');
+        }
+      }
+
+      locationData = await locationService.getLocation();
+    } on PlatformException catch (e) {
+      print('Error: ${e.toString()}, code: ${e.code}');
+      locationData = null;
+    }
+    locationData = await locationService.getLocation();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     if (image == null) {
       getImage();
+      retrieveLocation();
       return Center(child: CircularProgressIndicator());
     } else {
       return Scaffold(
@@ -46,7 +98,13 @@ class _AddPostState extends State<AddPost> {
                   ),
                   RaisedButton(
                     child: Text('Upload'),
-                    onPressed: () {},
+                    onPressed: () {
+                      uploadImage();
+                      FirebaseFirestore.instance.collection('posts').add({
+                        'weight': 222,
+                        'submission_date': DateTime.parse('2020-01-31')
+                      });
+                    },
                   )
                 ],
               )));
