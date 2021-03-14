00f248d3385ba5b2fbab48b41355368676247c11
Malgorzata Sinha <sklarekm@oregonstate.edu>
2021-03-13 17:18:12 -0800

    form working here

--- lib/screens/add_post.dart
+++ lib/screens/add_post.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter/services.dart';
 import 'package:location/location.dart';
+import 'package:wasteagram/models/post_details.dart';
 import '../location.dart';
 import 'package:path/path.dart' as Path;
 
@@ -17,11 +18,23 @@ class _AddPostState extends State<AddPost> {
   File image;
   LocationData locationData;
   final picker = ImagePicker();
+  final formKey = new GlobalKey<FormState>();
+  final post = PostDetails();
 
   @override
   void initState() {
     super.initState();
     retrieveLocation();
+  }
+
+  //Validate if form is valid and Save
+  bool validateAndSave() {
+    final form = formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      return true;
+    }
+    return false;
   }
 
   Future getImage() async {
@@ -84,30 +97,70 @@ class _AddPostState extends State<AddPost> {
     } else {
       return Scaffold(
           body: Container(
-              padding: const EdgeInsets.all(40.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.file(image),
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Enter weight'),
-                    keyboardType: TextInputType.numberWithOptions(),
-                    inputFormatters: <TextInputFormatter>[
-                      FilteringTextInputFormatter.digitsOnly
-                    ],
-                  ),
-                  RaisedButton(
-                    child: Text('Upload'),
-                    onPressed: () {
-                      // uploadImage();
-                      // FirebaseFirestore.instance.collection('posts').add({
-                      //   'weight': 222,
-                      //   'submission_date': DateTime.parse('2020-01-31')
-                      // });
-                    },
-                  )
-                ],
-              )));
+              child: Form(
+                  key: formKey,
+                  child: ListView(children: <Widget>[
+                    Image.file(
+                      image,
+                      height: 330.0,
+                      width: 660,
+                    ),
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.number,
+                      inputFormatters: <TextInputFormatter>[
+                        WhitelistingTextInputFormatter.digitsOnly
+                      ], // Only numbers can be entered
+                      decoration:
+                          new InputDecoration(labelText: 'Number of Items'),
+                      onSaved: (value) {
+                        return post.weight = num.parse(value);
+                      },
+                    ),
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    Semantics(
+                      button: true,
+                      enabled: true,
+                      onTapHint: 'Upload the Item',
+                      child: RaisedButton(
+                        elevation: 10.0,
+                        child: Text('Add a new Item'),
+                        textColor: Colors.white,
+                        color: Colors.pink,
+                        onPressed: () {
+                          uploadImage();
+                        },
+                      ),
+                    ),
+                  ]))));
+      // padding: const EdgeInsets.all(40.0),
+      // child: Column(
+      //   mainAxisAlignment: MainAxisAlignment.center,
+      //   children: [
+      //     Image.file(image),
+      //     TextField(
+      //       decoration: InputDecoration(labelText: 'Enter weight'),
+      //       keyboardType: TextInputType.numberWithOptions(),
+      //       inputFormatters: <TextInputFormatter>[
+      //         FilteringTextInputFormatter.digitsOnly
+      //       ],
+      //     ),
+      //     RaisedButton(
+      //       child: Text('Upload'),
+      //       onPressed: () {
+      //         // uploadImage();
+      //         // FirebaseFirestore.instance.collection('posts').add({
+      //         //   'weight': 222,
+      //         //   'submission_date': DateTime.parse('2020-01-31')
+      //         // });
+      //       },
+      //     )
+      //   ],
+      // )));
     }
   }
 }
