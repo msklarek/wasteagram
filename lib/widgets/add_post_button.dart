bc066ad42bc7ec95c2ac542ea4b425946ed20caa
Malgorzata Sinha <sklarekm@oregonstate.edu>
2021-03-14 15:57:21 -0700

    adding semantic widget

--- lib/widgets/add_post_button.dart
+++ lib/widgets/add_post_button.dart
@@ -4,13 +4,17 @@ import 'package:wasteagram/screens/add_post.dart';
 class AddPostButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FloatingActionButton(
-      onPressed: () {
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => AddPost()));
-      },
-      tooltip: 'Add Entry',
-      child: Icon(Icons.camera),
-    );
+    return Semantics(
+        button: true,
+        enabled: true,
+        onTapHint: 'Add a new post',
+        child: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => AddPost()));
+          },
+          tooltip: 'Add Post',
+          child: Icon(Icons.camera),
+        ));
   }
 }
