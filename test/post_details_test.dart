cc68cacfe0d7f6e6533e2e8d2cd441556dea0685
Malgorzata Sinha <sklarekm@oregonstate.edu>
2021-03-14 22:01:59 -0700

    changed to test

--- test/post_details_test.dart
+++ test/post_details_test.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_firestore_mocks/cloud_firestore_mocks.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:wasteagram/models/post_details.dart';
+import 'package:wasteagram/screens/add_post.dart';
 
 void main() async {
   int currentTimeMs = DateTime.now().millisecondsSinceEpoch;
@@ -43,5 +44,13 @@ void main() async {
     expect(post.location.latitude, 22.6);
     expect(post.location.longitude, 23.5);
     expect(post.weight, 2);
+  });
+
+  test('Empty value should return error', () {
+    expect(Validator.validateNum(''), 'Please enter a value');
+  });
+
+  test('Valid value should not return error', () {
+    expect(Validator.validateNum('5'), null);
   });
 }
